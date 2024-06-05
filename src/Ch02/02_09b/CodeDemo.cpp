2a86987d21140eb4dc5a2f96b69347df4d69b55b
anonymous2308 <45162950+anonymous2308@users.noreply.github.com>
2024-06-05 09:24:23 +0000

    Changed

--- src/Ch02/02_09b/CodeDemo.cpp
+++ src/Ch02/02_09b/CodeDemo.cpp
@@ -3,8 +3,18 @@
 // Strings, by Eduardo Corpeño 
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 int main(){
+    const size_t LENGTH=25;
+    char array[LENGTH] = "Hey there! ";
+    char array1[]= "Good Morning";
+    strcat(array, array1);
+    std::string str= "Have a great day! ";
+    std::string str1= "Tadowww";
+    std::cout << array << std::endl;
+    std::cout << str+str1 << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
