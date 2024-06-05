b57802004a181b132da9187c1a82de232212d009
anonymous2308 <45162950+anonymous2308@users.noreply.github.com>
2024-06-05 10:24:33 +0000

    Changed

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -5,8 +5,20 @@
 #include <iostream>
 
 int main(){
-    int nums[5] = {1,23,32,24,337};
-    float result; 
+    float a,b,c,d,e;
+    std::cout << "Number 1: " << std::flush;
+    std::cin >> a;
+    std::cout << "Number 2: " << std::flush;
+    std::cin >> b;
+    std::cout << "Number 3: " << std::flush;
+    std::cin >> c;
+    std::cout << "Number 4: " << std::flush;
+    std::cin >> d;
+    std::cout << "Number 5: " << std::flush;
+    std::cin >> e;
+    float nums[5] = {a,b,c,d,e};
+    float result;
+    result= (nums[0]+nums[1]+nums[2]+nums[3]+nums[4])/5; 
 
     // Write your code here
     
