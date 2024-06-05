51ff397b768904a201ebd4a03b9141432fee7c2d
anonymous2308 <45162950+anonymous2308@users.noreply.github.com>
2024-06-05 09:04:40 +0000

    Changes

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,30 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+#include <string>
+bool x;
+float a,b;
+int main(){
+    std::string str;
+    std::cout << "Welcome, Enter Your Name: " << std::flush;
+    std::cin >> str;
+    std::cout << "Hello " << str << std::endl;
+    std::cout << "a = ";
+    std::cin >> a;
+    std::cout << "b = ";
+    std::cin >> b;
+    std::cout << "a + b = " << a+b << std::endl;
+    std::cout << "a - b = " << a-b << std::endl;
+    if ((a+b)>(a-b))
+    {
+        x=1;
+    }else{
+        x=0;
+    }
+    std::cout<< "Comparison= " << x << std::flush;
+    std::cout << std::endl << std::endl;
+    return(0);
+}
+
--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -4,9 +4,21 @@
 
 #include <iostream>
 #include <typeinfo>
-
+auto a=2;
+auto b=22343242;
+auto c=3.56f;
+auto d=124.5234;
+auto e=true;
+auto f="Hello";
+auto g="Hello world";
 int main(){
-    
+    std::cout << "The type of a is " << typeid(a).name() << std::endl;
+    std::cout << "The type of b is " << typeid(b).name() << std::endl;
+    std::cout << "The type of c is " << typeid(c).name() << std::endl;
+    std::cout << "The type of d is " << typeid(d).name() << std::endl;
+    std::cout << "The type of e is " << typeid(e).name() << std::endl;
+    std::cout << "The type of f is " << typeid(f).name() << std::endl;
+    std::cout << "The type of g is " << typeid(g).name() << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
