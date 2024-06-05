c4fe91f77060ae903761a2f94ff543a6c9f1dca7
anonymous2308 <45162950+anonymous2308@users.noreply.github.com>
2024-06-05 10:14:15 +0000

    Changed

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -4,9 +4,26 @@
 
 #include <iostream>
 #include <cstdint>
+#include <string>
 
 int main(){
-    
+    double f,c;
+    std::string str;
+    std::cout << "C or F: " << std::flush;
+    std::cin >> str;
+    if(str== "C"){
+        std::cout << "Celcius= " << std::flush;
+        std::cin >> c;
+        f=(c * 9.0f/5.0)+32;
+        std::cout << "Fahrenheit= " << f << std::endl;
+    }
+    else if (str== "F")
+    {
+        std::cout << "Fahrenheit= "  << std::flush;
+        std::cin >> f;
+        c = (f-32)*5.0f/9.0;
+        std::cout << "Celcius= " << c << std::endl; 
+    }   
     std::cout << std::endl << std::endl;
     return (0);
 }
