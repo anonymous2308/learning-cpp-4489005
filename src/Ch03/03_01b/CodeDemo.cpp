24b69b96cf05b0a647253bbe4584e29a24bf8f3b
anonymous2308 <45162950+anonymous2308@users.noreply.github.com>
2024-06-11 10:14:47 +0000

    Changes

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -4,9 +4,22 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
+enum class cow_purpose{pet,meat,dairy,hide};
+struct cow
+{                
+    string name;
+    int age;
+    cow_purpose purpose;
+    /* data */                  
+};
 
 int main(){
-    
+    cow my_cow;
+    my_cow.age=21;
+    my_cow.name = "XYZ";
+    my_cow.purpose= cow_purpose::pet;
+    cout << my_cow.name << " is a type- " << int(my_cow.purpose) << "Cow who is of age " << my_cow.age << endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
